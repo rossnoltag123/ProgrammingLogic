f29342ac54129577b1290382a4454ab1cea1ac82
gelmisb <gelmisb@gmail.com>
2017-10-18 14:32:11 +0100

    Reactor re-work and UI changes

---
+++ Assets/SBSP-B2-engineering-energy/Scripts/Energy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Energy{
+
+	public int energyUnit;
+	public string energyType;
+	public int energyQuantity;
+
+
+
+
+	public string GetEnergyType(){
+		return energyType;
+	}
+
+	public void SetEnergyType(string energyType){
+		this.energyType = energyType;
+	}
+
+
+
+
+	public int GetEnergyUnit(){
+		return energyUnit;
+	}
+
+	public void SetEnergyUnit(int energyUnit){
+		this.energyUnit = energyUnit;
+	}
+
+
+
+
+
+	public int GetEnergyQuantity(){
+		return energyQuantity;
+	}
+
+	public void SetEnergyQuantity(int energyQuantity){
+		this.energyQuantity = energyQuantity;
+	}
+
+}
