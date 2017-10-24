3fc5795173cda27d012756e5d0cc03b5da21b34e
Adem Slavotic <SlavoticA@Adems-MacBook-Pro.local>
2017-10-24 20:59:20 +0100

    UI script updated, switch between scenes, movement etc

---
+++ Assets/SBSP-B2-engineering-energy/Scripts/MenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+	
+	string distributionScene = "eng-test-scene";
+	string reactorChamberScene = "reactor-test-system";
+
+	public void GoToEnergyDistributions(){
+		Application.LoadLevel (distributionScene);
+	}
+
+	public void GoToEnergyReactorChamber(){
+		Application.LoadLevel (reactorChamberScene);
+	}
+
+}
